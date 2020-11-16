402983af862fc29841fc15fd1c4c6b911b7bbb64
thecodingconductor <tristan.raissherman@gmail.com>
2020-11-16 14:52:13 -0500

    prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path')
 
 const app = express();
 
@@ -7,13 +8,19 @@ connectDB();
 
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => res.json({ msg: "welcome to the American Orchestra People Book" }))
+
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/organizations', require('./routes/organizations'));
 
+//Serve static assest in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
