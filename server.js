052fe139a97cd5d784e6cd8f1a8ac771ba5b397d
thecodingconductor <tristan.raissherman@gmail.com>
2020-11-16 15:02:02 -0500

    redo

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ connectDB();
 
 app.use(express.json({ extended: false }))
 
-
-
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
